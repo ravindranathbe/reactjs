3fd6502e47ddb5603ecb51fd386d8dc698b3f10e
Ravindranath P <ravindranathbe@gmail.com>
2017-03-19 12:15:29 +0530

    combine multiple reducer code

--- src/app/index3.js
+++ src/app/index3.js
@@ -28,7 +28,7 @@ class App extends React.Component {
 
 render(<App/>, document.getElementById('app'));*/
 
-import {createStore} from "redux";
+import {createStore, combineReducers} from "redux";
 
 const initialState = {
 	result: 1,
@@ -36,7 +36,7 @@ const initialState = {
 	username: 'Max'
 }
 
-const reducer = (state = initialState, action) => {
+const mathReducer = (state = initialState, action) => {
 	switch(action.type) {
 		case "ADD":
 			// state = state + action.payload;
@@ -66,7 +66,28 @@ const reducer = (state = initialState, action) => {
 	return state;
 };
 
-const store = createStore(reducer);
+const userReducer = (state = {
+	name: 'Max',
+	age: 27
+}, action) => {
+	switch(action.type) {
+		case "SET_NAME":
+			state = {
+				...state,
+				name: action.payload
+			}
+			break;
+		case "SET_AGE":
+			state = {
+				...state,
+				age: action.payload
+			}
+			break;
+	}
+	return state;
+};
+
+const store = createStore(combineReducers({mathReducer, userReducer}));
 
 store.subscribe(() => {
 	console.log('Store updated', store.getState());
@@ -85,4 +106,9 @@ store.dispatch({
 store.dispatch({
 	type: "SUBTRACT",
 	payload: 80
+});
+
+store.dispatch({
+	type: "SET_NAME",
+	payload: "Nancy"
 });
